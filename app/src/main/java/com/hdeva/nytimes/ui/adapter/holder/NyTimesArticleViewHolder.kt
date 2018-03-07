9e0aef2152831df37e4fb11d47cd76f2a49ca1d5
hundeva <hundeva@gmail.com>
2018-03-07 15:38:54 +0100

    redirect item taps to browser

--- app/src/main/java/com/hdeva/nytimes/ui/adapter/holder/NyTimesArticleViewHolder.kt
+++ app/src/main/java/com/hdeva/nytimes/ui/adapter/holder/NyTimesArticleViewHolder.kt
@@ -1,13 +1,27 @@
 package com.hdeva.nytimes.ui.adapter.holder
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import com.hdeva.nytimes.databinding.ItemNyTimesArticleBinding
 import com.hdeva.nytimes.model.NyTimesArticle
 
 class NyTimesArticleViewHolder(val binding: ItemNyTimesArticleBinding) : RecyclerView.ViewHolder(binding.root) {
 
+    private var article: NyTimesArticle? = null
+
+    init {
+        binding.itemNyTimesArticleContainer.setOnClickListener { openArticle() }
+    }
 
     fun bind(article: NyTimesArticle) {
-        binding.itemNyTimesArticleTitle.text = article.title
+        this.article = article
+        with(binding) {
+            itemNyTimesArticleTitle.text = article.title
+        }
+    }
+
+    private fun openArticle() {
+        binding.root.context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(article?.url)))
     }
 }
