b8067001052e9ad62917a546af2337363ecbda15
hundeva <hundeva@gmail.com>
2018-03-07 12:15:11 +0100

    replace default launcher icon add basic theming add databinding

--- app/src/main/java/com/hdeva/nytimes/arch/base/BaseActivity.kt
+++ app/src/main/java/com/hdeva/nytimes/arch/base/BaseActivity.kt
@@ -1,6 +1,12 @@
 package com.hdeva.nytimes.arch.base
 
+import com.hdeva.nytimes.R
 import dagger.android.support.DaggerAppCompatActivity
 
 abstract class BaseActivity : DaggerAppCompatActivity() {
+
+    override fun onContentChanged() {
+        super.onContentChanged()
+        setSupportActionBar(findViewById(R.id.support_toolbar))
+    }
 }
--- app/src/main/java/com/hdeva/nytimes/ui/home/HomeActivity.kt
+++ app/src/main/java/com/hdeva/nytimes/ui/home/HomeActivity.kt
@@ -1,13 +1,18 @@
 package com.hdeva.nytimes.ui.home
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import com.hdeva.nytimes.R
 import com.hdeva.nytimes.arch.base.BaseActivity
+import com.hdeva.nytimes.databinding.ActivityHomeBinding
 
 class HomeActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.NyTimes_Theme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
     }
 }
