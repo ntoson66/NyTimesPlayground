0140288505f710c4d60228e7ae3db48ddebdbf5b
hundeva <hundeva@gmail.com>
2018-03-11 17:50:48 +0100

    cleanup

--- app/src/main/java/com/hdeva/nytimes/NyTimesApp.kt
+++ app/src/main/java/com/hdeva/nytimes/NyTimesApp.kt
@@ -1,12 +1,11 @@
 package com.hdeva.nytimes
 
 import com.hdeva.nytimes.arch.di.DaggerApplicationComponent
-import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import io.paperdb.Paper
 
-class NyTimesApp: DaggerApplication() {
+class NyTimesApp : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.builder().create(this)
@@ -14,7 +13,6 @@ class NyTimesApp: DaggerApplication() {
 
     override fun onCreate() {
         Paper.init(this)
-        AndroidThreeTen.init(this)
         super.onCreate()
     }
 }
